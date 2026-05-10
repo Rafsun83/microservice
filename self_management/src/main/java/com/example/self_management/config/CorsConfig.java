170268f7df23ea3b48bdf099a16d4a979e1b84a0
rafsun-jani-qp <rafsun.jani@question.com>
2026-05-11 00:30:46 +0600

    Update

--- self_management/src/main/java/com/example/self_management/config/CorsConfig.java
+++ self_management/src/main/java/com/example/self_management/config/CorsConfig.java
@@ -13,7 +13,8 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+//        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(true);
